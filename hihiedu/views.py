9a46bc320e23bc4d85e707967ff469547a528068
chenxu981110 <1845042363@qq.com>
2020-12-30 14:45:45 +0800

    first commit

---
+++ hihiedu/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+from django.shortcuts import render
+
+# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
+class IndexView(TemplateView):
+    template_name = 'index.html'
